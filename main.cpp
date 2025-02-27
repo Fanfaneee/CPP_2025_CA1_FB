63cd9275d8c80a56d2adba20589a5a982eaee99a
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-02-27 21:59:41 +0000

    menu finished

--- main.cpp
+++ main.cpp
@@ -187,15 +187,117 @@ void sortByPriceDescending(vector<projectCrochet> &projects) {
     display(projects);
 }
 
+void question1(const vector<projectCrochet> &projects) {
+    display(projects);
+}
+
+void question2(const vector<projectCrochet> &projects) {
+    string nameSearch;
+    cout << "Enter the exact name of the project you are searching for: ";
+    getline(cin, nameSearch);
+
+    int index = findIndexOf(projects, nameSearch);
+    if (index != -1) {
+        cout << "Project found:" << endl;
+        display({projects[index]});
+    } else {
+        cout << "Project not found." << endl;
+    }
+}
+
+void question3(vector<projectCrochet> &projects) {
+    map<string, int> countYarnType = mapOfYarnType(projects);
+    cout << "Number of projects by yarn type :" << endl;
+
+
+    map<string, int>::iterator iter;
+    for (iter = countYarnType.begin(); iter != countYarnType.end(); iter++) {
+        cout << iter->first << " : " << iter->second << endl;
+    }
+}
+
+
+void question4(const vector<projectCrochet> &projects) {
+    string categorySearch;
+    cout << "Enter a final use (Blanket, Baby, Home Decor, Accessory, Kitchen): ";
+    getline(cin, categorySearch);
+
+    vector<projectCrochet> filteredProjects = filterByFinalUse(projects, categorySearch);
+    if (!filteredProjects.empty()) {
+        cout << "Projects matching the final use '" << categorySearch << "':" << endl;
+        display(filteredProjects);
+    } else {
+        cout << "No project found for the final use '" << categorySearch << "'." << endl;
+    }
+}
+
+void question5(const vector<projectCrochet> &projects) {
+    int avgTime;
+    projectCrochet minProject, maxProject;
+
+    analyzeTimeOfProjects(projects, avgTime, minProject, maxProject);
+
+    cout << "Average projects time: " << avgTime << " hours" << endl;
+    cout << "Fastest project: " << minProject.name << " (" << minProject.time << " hours)" << endl;
+    cout << "Longest project: " << maxProject.name << " (" << maxProject.time << " hours)" << endl;
+}
+
+void question6(vector<projectCrochet> &projects) {
+    string stringToMatch;
+    cout << "Enter the partial name of a project (Amigurumi, Crochet, Blanket...): ";
+    getline(cin, stringToMatch);
+
+    vector<projectCrochet> projectsMatchName = matchName(projects, stringToMatch);
+    display(projectsMatchName);
+}
+
+void question7(vector<projectCrochet> &projects) {
+    cout << "List of projects by descending price:" << endl;
+    sortByPriceDescending(projects);
+}
+
 
 int main() {
+
     vector<projectCrochet> projects;
     string filename = "./cmake-build-debug/data.csv";
     load(filename,projects);
 
-// Question 1 display:
+    int choice;
 
+    do {
+        cout << "******MENU******" << endl;
+        cout << "1. Display all projects" << endl;
+        cout << "2. Search for a project by exact name" << endl;
+        cout << "3. Count projects by yarn type" << endl;
+        cout << "4. Filter projects by final use" << endl;
+        cout << "5. Analyze project times" << endl;
+        cout << "6. Search for projects by partial name" << endl;
+        cout << "7. Sort and display projects by descending price" << endl;
+        cout << "-1. Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+            case 1: question1(projects); break;
+            case 2: question2(projects); break;
+            case 3: question3(projects); break;
+            case 4: question4(projects); break;
+            case 5: question5(projects); break;
+            case 6: question6(projects); break;
+            case 7: question7(projects); break;
+            case -1: cout << "Exiting program." << endl; break;
+            default: cout << "Invalid option, try again." << endl;
+        }
+
+    } while (choice != -1);
+
+
+// Question 1 display:
+    /*
     display(projects);
+
 
 //question 2 findIndexOf       search the exact name and return the project (fonction return int)
     string nameSearch;
@@ -236,7 +338,7 @@ int main() {
     }
 
 
-//question 5
+//question 5 time by project
 
     int avgTime;
     projectCrochet minProject, maxProject;
@@ -259,11 +361,11 @@ int main() {
     display(projectsMatchName);
 
 
-//question 7
+//question 7   List of project by descending price
     cout<<"List of project by descending price"<<endl;
     sortByPriceDescending(projects);
 
-
+*/
 
     return 0;
 
